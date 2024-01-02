4867162b4ca204cd23a0474765fb7c62bafab06a
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-01-02 22:42:08 +0100

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,8 +4,8 @@ const Button = ({text, onClick}) => {
     return (<button onClick={onClick}>{text}</button>)
 }
 
-const Stat = ({name, value}) => {
-    return (<p>{name} {value}</p>)
+const Stat = ({name, value, unit=''}) => {
+    return (<p>{name} {value} {unit}</p>)
 }
 
 const App = () => {
@@ -13,17 +13,43 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [average, setAverage] = useState(0.0)
+    const [positivePct, setPositivePct] = useState(0.0)
 
     return (
         <div>
             <h1>give feedback</h1>
-            <Button text={'good'} onClick={() => setGood(good + 1)}/>
-            <Button text={'neutral'} onClick={() => setNeutral(neutral + 1)}/>
-            <Button text={'bad'} onClick={() => setBad(bad + 1)}/>
+            <Button text={'good'} onClick={() => {
+                const newGood = good + 1
+                setGood(newGood)
+                const newTotal = total + 1
+                setTotal(newTotal)
+                setAverage((newGood - bad) / newTotal)
+                setPositivePct(newGood / newTotal * 100)
+            }}/>
+            <Button text={'neutral'} onClick={() => {
+                setNeutral(neutral + 1)
+                const newTotal = total + 1
+                setTotal(newTotal)
+                setAverage((good - bad) / newTotal)
+                setPositivePct(good / newTotal * 100)
+            }}/>
+            <Button text={'bad'} onClick={() => {
+                const newBad = bad + 1
+                setBad(newBad)
+                const newTotal = total + 1
+                setTotal(newTotal)
+                setAverage((good - newBad) / newTotal)
+                setPositivePct(good / newTotal * 100)
+            }}/>
             <h1>statistics</h1>
             <Stat name={'good'} value={good}/>
             <Stat name={'neutral'} value={neutral}/>
             <Stat name={'bad'} value={bad}/>
+            <Stat name={'all'} value={total}/>
+            <Stat name={'average'} value={average}/>
+            <Stat name={'positive'} value={positivePct} unit={'%'}/>
         </div>
     )
 }
