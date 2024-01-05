ab49d8808954454555b17f2b2810c95e9a33995e
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-01-05 19:37:03 +0100

    2.4: Course information step9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -18,7 +18,7 @@ const Content = ({content}) => {
     const result = []
     content.forEach((part) => {
         result.push(
-            <Part key={part.id} part={part} />
+            <Part key={part.id} part={part}/>
         )
     })
     return result
@@ -41,32 +41,61 @@ const Course = ({course}) => {
     )
 }
 
-
 const App = () => {
 
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-    return <Course course={course} />
+    const courseComponents = []
+    courses.forEach((course) => courseComponents.push(<Course key={course.id} course={course}/>))
+
+    return (
+        <div>
+            {courseComponents}
+        </div>
+    )
 }
 
 export default App
